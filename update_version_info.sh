d7ba55bdd74a6d65ec4be870fbdf37223aca1826
hydazz <alexanderhyde@icloud.com>
2021-01-15 00:30:00 +1100

    initial commit to please codacy

--- update_version_info.sh
+++ update_version_info.sh
@@ -2,8 +2,8 @@
 
 OVERLAY_VERSION=$(curl -sX GET "https://raw.githubusercontent.com/hydazz/docker-baseimage-alpine-arr/main/version_info.json" | jq -r .overlay_version)
 
-OLD_OVERLAY_VERSION=$(cat version_info.json | jq -r .overlay_version)
-OLD_LIDARR_RELEASE=$(cat version_info.json | jq -r .lidarr_release)
+OLD_OVERLAY_VERSION=$(jq <version_info.json -r .overlay_version)
+OLD_LIDARR_RELEASE=$(jq <version_info.json -r .lidarr_release)
 
 sed -i \
 	-e "s/${OLD_OVERLAY_VERSION}/${OVERLAY_VERSION}/g" \
